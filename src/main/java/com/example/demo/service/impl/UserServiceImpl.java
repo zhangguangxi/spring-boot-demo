70b7089e6968c709f4d496185739730a9ef920b2
zhangguangxi <zhangguangxito@sina.cn>
2017-07-25 17:54:07 +0800

    spring boot 集成jdbc

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,4 +1,3 @@
-/*
 package com.example.demo.service.impl;
 
 import com.example.demo.service.UserService;
@@ -10,49 +9,40 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    */
-/**
+    /**
      * 新增一个用户
      *
      * @param name
      * @param age
-     *//*
-
+     */
     @Override
     public void create(String name, Integer age) {
-        jdbcTemplate.update("insert into USER(NAME, AGE) values(?, ?)", name, age);
+        jdbcTemplate.update("insert into user(name, age) values(?, ?)", name, age);
     }
 
-    */
-/**
+    /**
      * 根据name删除一个用户高
      *
      * @param name
-     *//*
-
+     */
     @Override
     public void deleteByName(String name) {
-        jdbcTemplate.update("delete from USER where NAME = ?", name);
+        jdbcTemplate.update("delete from user where NAME = ?", name);
     }
 
-    */
-/**
+    /**
      * 获取用户总量
-     *//*
-
+     */
     @Override
     public Integer getAllUsers() {
-        return jdbcTemplate.queryForObject("select count(1) from USER", Integer.class);
+        return jdbcTemplate.queryForObject("select count(1) from user", Integer.class);
     }
 
-    */
-/**
+    /**
      * 删除所有用户
-     *//*
-
+     */
     @Override
     public void deleteAllUsers() {
-        jdbcTemplate.update("delete from USER");
+        jdbcTemplate.update("delete from user");
     }
 }
-*/
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,11 @@
-/*
 package com.example.demo;
 
+import com.example.demo.service.UserService;
 import com.example.demo.web.BlogProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,17 @@ public class DemoApplicationTests {
 	private  static final Log log = LogFactory.getLog(DemoApplicationTests.class);
 	@Autowired
 	private BlogProperties blogProperties;
+
+	@Autowired
+    private UserService userService;
+
+	@Before
+    public void setUp()
+    {
+        /**准备，清空user表*/
+        userService.deleteAllUsers();
+    }
+
 	@Test
 	public void contextLoads() {
 	}
@@ -31,13 +43,29 @@ public class DemoApplicationTests {
 
 		log.info("随机数测试输出：");
 		log.info("随机字符串 : " + blogProperties.getValue());
-		log.info("随机int : " + blogProperties.getNumber());
-		log.info("随机long : " + blogProperties.getBignumber());
+		log.info("随机int : "   + blogProperties.getNumber());
+		log.info("随机long : "  + blogProperties.getBignumber());
 		log.info("随机10以下 : " + blogProperties.getTest1());
-		log.info("随机10-20 : " + blogProperties.getTest2());
-
+		log.info("随机10-20 : "  + blogProperties.getTest2());
 	}
 
 
+    @Test
+    public void test() throws Exception {
+        // 插入5个用户
+        userService.create("a", 1);
+        userService.create("b", 2);
+        userService.create("c", 3);
+        userService.create("d", 4);
+        userService.create("e", 5);
+        // 查数据库，应该有5个用户
+        Assert.assertEquals(5, userService.getAllUsers().intValue());
+        // 删除两个用户
+        userService.deleteByName("a");
+        userService.deleteByName("e");
+        // 查数据库，应该有5个用户
+        Assert.assertEquals(3, userService.getAllUsers().intValue());
+    }
+
+
 }
-*/
